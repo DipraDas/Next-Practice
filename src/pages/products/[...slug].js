656a12276efb0175d5a5df45686a9d79c26d8055
Dipra <dipradas5940@gmail.com>
2024-04-01 21:48:09 +0600

    Slug file and 404 page concept

---
+++ src/pages/products/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CacheAllRoutes = () => {
+    return (
+        <div>
+            <h1>This is for cache files</h1>
+        </div>
+    );
+};
+
+export default CacheAllRoutes;
